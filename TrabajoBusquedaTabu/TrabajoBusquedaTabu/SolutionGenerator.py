3ef32a70ffcf685fb42791f949c660ed2803c79d
Alba Carmona Jamardo <albacarmona91@gmail.com>
2017-01-02 20:30:40 +0100

    Creada la clase SolutionGenerator

---
+++ TrabajoBusquedaTabu/TrabajoBusquedaTabu/SolutionGenerator.py
@@ -0,0 +1,10 @@
+import numpy
+
+# link de ayuda para el código de las 4 distribuciones:
+# http://relopezbriega.github.io/blog/2016/06/29/distribuciones-de-probabilidad-con-python/
+
+class SolutionGenerator(object):
+    """description of class"""
+    pass
+
+
