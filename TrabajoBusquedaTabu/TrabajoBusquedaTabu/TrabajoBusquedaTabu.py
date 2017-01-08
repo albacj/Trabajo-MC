c814ef6cfe7c27ab9b9d1d7419b2e75af91522db
andopr <andreeamoprescu@gmail.com>
2017-01-08 14:35:26 +0100

    Solucion actualizada

--- TrabajoBusquedaTabu/TrabajoBusquedaTabu/TrabajoBusquedaTabu.py
+++ TrabajoBusquedaTabu/TrabajoBusquedaTabu/TrabajoBusquedaTabu.py
@@ -3,8 +3,24 @@
 #import Solucion
 #import SolutionGenerator
 import AlgoritmoTabu
+import numpy as np
+
+a = 2
+w = np.random.weibull(a, 16)
+#print (w)
 
 
+print ("Matematicas para la Computacion >> Curso 2016/2017")
+print ("")
+print ("Algoritmo Tabu aplicado a problema de emplazamiento de nodos")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Alumnas: Alba Carmona Jamardo y Andreea Oprescu")
+print("Docente: Maria Isabel Hartillo Hermoso")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Por favor, introduzca los siguientes parametros para ejecutar el algoritmo")
+print("")
+gridSize = input("Tamano del tablero:")
+distributionType = input("Tipo de distribucion: ")
 #for row in sol.adjMatrix:
 #	print(row)
 #ccs = sol.getConnectedComponents(len(routersPosition), sol.adjMatrix)
@@ -13,6 +29,7 @@ import AlgoritmoTabu
 #users = sol.connectedUsers
 #print(giant, users)
 
-tabuSearch = AlgoritmoTabu.AlgoritmoTabu(8,8,4,12,1,0)
-best = tabuSearch.TabuSearch()
-print(best)
+#tabuSearch = AlgoritmoTabu.AlgoritmoTabu(8,8,4,12,1,0)
+#best = tabuSearch.TabuSearch()
+#print(best)
+
